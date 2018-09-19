8759c459472ef26411d7743a64016cbba567b5ad
Denis Vashchuk <vashchuk.denis@gmail.com>
2018-09-19 15:17:32 +0300

    add hello.test.js

---
+++ tests/hello.test.js
@@ -0,0 +1,23 @@
+const EOSTest = require('..');
+
+it('tests hello contract', async (done) => {
+  const eosTest = new EOSTest();
+  console.log('init');
+  await eosTest.init();
+  console.log('build contract');
+  await eosTest.make('./contracts/hello');
+  console.log('create test account');
+  await eosTest.newAccount('test');
+  console.log('create hello account');
+  await eosTest.newAccount('hello');
+  console.log('deploy contract');
+  const contract = await eosTest.deploy('hello', '../contracts/hello/hello.wasm', '../contracts/hello/hello.abi');
+  console.log('call contract methods');
+  await contract.hi('test', {authorization: 'test'});
+
+  await eosTest.destroy();
+  console.log(process._getActiveRequests());
+  console.log(process._getActiveHandles());
+  done();
+}, 30000);
+
